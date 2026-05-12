1be8d9847738a5ced1e23ce14a18fd63fa90e36e
Cristofer Figueroa <cristo.figueroam@duocuc.cl>
2026-05-11 21:44:24 -0400

    Add files via upload

---
+++ MS-Vendedores (1)/MS-Vendedores/src/main/java/com/AutoMaster/MS_Vendedores/MsVendedoresApplication.java
@@ -0,0 +1,13 @@
+package com.AutoMaster.MS_Vendedores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsVendedoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsVendedoresApplication.class, args);
+	}
+
+}
---
+++ MS-Vendedores (1)/MS-Vendedores/src/test/java/com/AutoMaster/MS_Vendedores/MsVendedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AutoMaster.MS_Vendedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsVendedoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
